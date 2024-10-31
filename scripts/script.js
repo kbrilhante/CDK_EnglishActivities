2b8da715e5ef527eb69a8361b8fce234913fb142
Karina <kbrilhante@gmail.com>
2024-10-31 00:19:39 -0300

    modified:   data/activities.json 	modified:   index.html 	modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,7 @@ function initialize() {
     const JSONDATA = "./data/activities.json";
     getData(JSONDATA).then((data) => {
         const info = processData(data);
-        createSelector(info);
     });
-    document.getElementById("btnCopy").onclick = copyText;
-    document.getElementById("btnDownload").onclick = downloadText;
 }
 
 async function getData(jsonFile) {
@@ -14,9 +11,25 @@ async function getData(jsonFile) {
     return data;
 }
 
+function processData(data) {
+    const container = document.getElementById("lstActivities");
+    const listGroup = document.createElement("div");
+    listGroup.className = "list-group";
+    container.appendChild(listGroup);
+    for (const info of data) {
+        listGroup.appendChild(listItem(info));
+    }
+}
 
-
-
+function listItem(info) {
+    console.log(info)
+    const link = document.createElement("a");
+    link.href = info.url;
+    link.target = "_blank";
+    link.className = "list-group-item list-group-item-action";
+    link.innerText = info.title;
+    return link;
+}
 
 
 initialize();
