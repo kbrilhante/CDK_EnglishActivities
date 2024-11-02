f642c19d9f39ea4013a071b67b96fc1ad297708c
Karina <kbrilhante@gmail.com>
2024-11-02 14:32:04 -0300

    modified:   backupJSON.bat 	modified:   data/40jf8Ikk - conversation-squad.json

--- backupJSON.bat
+++ backupJSON.bat
@@ -1,1 +1,3 @@
+xcopy "C:\Users\kbril\Downloads\40jf8Ikk - conversation-squad.json" "G:\Meu Drive\CDK\codigosDaKahProjetos\CDK_EnglishActivities\data" /E /I /y 
+del "C:\Users\kbril\Downloads\40jf8Ikk - conversation-squad.json"
 xcopy "G:\Meu Drive\CDK\codigosDaKahProjetos\CDK_EnglishActivities\data\activities.json" "G:\Meu Drive\CDK\codigosDaKahProjetos\CDK_EnglishActivitiesList" /E /I /y 
